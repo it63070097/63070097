10c27ab515fe22d62fd39f31b11803487c066d5c
Butthachai <73687978+it63070097@users.noreply.github.com>
2020-11-19 19:59:47 +0700

    Update forminsert.php

--- forminsert.php
+++ forminsert.php
@@ -9,7 +9,7 @@
     NAME(ENGLISH ONLY!):<br>
     <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
     กำลังทำอะไร<br>
-    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
+    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Your doing"></textarea><br>  
     <input type="submit" id="commentBtn">
   </form> 
 </body>
