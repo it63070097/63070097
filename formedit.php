8ffcc54238bb583615ae763c942768afef83e600
Butthachai <73687978+it63070097@users.noreply.github.com>
2020-11-19 18:58:23 +0700

    Add files via upload

---
+++ formedit.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Edit Form</title>
+<meta charset="utf-8">
+</head>
+<body>
+  <form action = "edit.php" method = "post" id="CommentForm" >
+    What name you want to change?:<br>
+    <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
+    New emotion:<br>
+    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter your feel"></textarea><br>  
+    <input type="submit" id="commentBtn">
+  </form> 
+</body>
+</html>
